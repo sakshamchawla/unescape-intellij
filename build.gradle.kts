f0a3f108534e6e69452ba4a45282ad28f218a1cf
saksham.chawla <saksham.chawla@macys.com>
2025-08-21 13:23:07 -0400

    updated build gradle and plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,31 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.23"
-  id("org.jetbrains.intellij") version "1.17.2"
+    id("org.jetbrains.intellij.platform") version "2.7.2"
+    kotlin("jvm") version "2.2.10"
 }
 
 group = "com.sakshamc"
-version = "0.0.6"
+version = "0.0.8"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-  version.set("2025.1")
-  type.set("IC") // Target IDE Platform
-
-  plugins.set(listOf(/* Plugin Dependencies */))
+repositories {
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2025.2")
+    }
+}
+
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
-  }
-
-  patchPluginXml {
-    sinceBuild.set("232")
-    untilBuild.set("242.*")
-  }
-
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
-
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    patchPluginXml {
+        sinceBuild.set("232")
+        untilBuild.set("300.*")
+    }
 }
